6a55ef2059e4372bddb962f2a788eda5cd802936
aicha <aicha.mbengue@gmail.com>
2013-07-18 18:14:03 +0200

    first change thisafternoon

---
+++ HotelDatabase/src/com/javaworld/hotels/model/HotelModelTest.java
@@ -0,0 +1,12 @@
+package com.javaworld.hotels.model;
+
+import junit.framework.TestCase;
+
+public class HotelModelTest extends TestCase {
+	 
+	public void testFailure() throws Exception {
+		fail();
+		
+	}
+
+}
